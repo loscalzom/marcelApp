b20392f2c10131f4bcb835e408e0e5760b490343
loscalzom <marceloscalzo@gmail.com>
2024-10-11 20:01:15 -0300

    reparando render

--- src/Components/WorkspacesListRender.jsx
+++ src/Components/WorkspacesListRender.jsx
@@ -3,21 +3,19 @@ import { getWorkspaces  } from '../data'
 import { Link } from 'react-router-dom'
 
 
-const WorkspacesListRender = (id,workspaceTitle) =>     {
-
-const workspaces= getWorkspaces()
-      
-const workspaceList= workspaces.map(({id,workspaceTitle}) => 
-<li key={id} className='workspaceList'>
- {workspaceTitle}
-
-<Link className='button enter' to={`/workspace/${id}`}>Entrar</Link>
-
-
-
-  </li> )
-return workspaceList
-    
+const WorkspacesListRender = () => {
+  const workspaces = getWorkspaces()
+  
+  return (
+      <ul>
+          {workspaces.map(({ id, workspaceTitle }) => (
+              <li key={id} className='workspaceList'>
+                  {workspaceTitle}
+                  <Link className='button enter' to={`/workspace/${id}`}>Entrar</Link>
+              </li>
+          ))}
+      </ul>
+  )
 }
 
 export default WorkspacesListRender
